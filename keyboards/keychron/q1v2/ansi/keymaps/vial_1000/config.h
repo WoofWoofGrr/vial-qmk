c9b4af1b168f00d56fc20596462ece44de629495
Devon <76901281+WoofWoofGrr@users.noreply.github.com>
2024-08-11 14:12:56 +1200

    Custom vial_1000 layout

---
+++ keyboards/keychron/q1v2/ansi/keymaps/vial_1000/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBD, 0x92, 0xD6, 0x68, 0x31, 0x5A, 0xC4, 0x4B}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
